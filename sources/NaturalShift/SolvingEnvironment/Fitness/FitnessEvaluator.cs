8846c0baa2ca44e72d4c7aedfe55e06e3bb35c4c
supix <esposito.marce@gmail.com>
2016-12-09 22:48:43 +0100

    License header added to NaturalShift project files

--- sources/NaturalShift/SolvingEnvironment/Fitness/FitnessEvaluator.cs
+++ sources/NaturalShift/SolvingEnvironment/Fitness/FitnessEvaluator.cs
@@ -1,4 +1,20 @@
-﻿using NaturalShift.Model.ProblemModel;
+﻿// Copyright (c) 2016 - esposito.marce@gmail.com
+// This file is part of NaturalShift.
+// 
+// NaturalShift is free software: you can redistribute it and/or modify
+// it under the terms of the Affero GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// Foobar is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+using NaturalShift.Model.ProblemModel;
 using NaturalShift.SolvingEnvironment.Matrix;
 using System.Collections.Generic;
 using System.Linq;
