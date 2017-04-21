817ebaf1f3ec5baf718e0f4cf03f2a8e3411196d
supix <esposito.marce@gmail.com>
2017-04-21 22:14:08 +0200

    Metadata changed
    Version number is updated to the last release.
    nuspec metadata updated.

--- sources/NaturalShift/Properties/AssemblyInfo.cs
+++ sources/NaturalShift/Properties/AssemblyInfo.cs
@@ -27,11 +27,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NaturalShift")]
-[assembly: AssemblyDescription("A library useful to compute workshifts based on a genetic algoritm.")]
+[assembly: AssemblyDescription("An artificial-intelligence based engine to compute workshifts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("supix")]
 [assembly: AssemblyProduct("NaturalShift")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
